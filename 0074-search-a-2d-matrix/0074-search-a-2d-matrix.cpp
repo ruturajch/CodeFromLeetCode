a9a1cf923b8b4a57a1386e4846f9c2f1bb1f9e6a
Ruturaj Chintawar <52837894+bitscoder@users.noreply.github.com>
2023-08-17 22:52:09 -0700

    Time: 3 ms (75.61%), Space: 9.5 MB (27.43%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool binary(vector<vector<int>>&  nums, int target, int i, int j, int n){
+        if(i < j){
+            int m = (j + i)/2;
+            if(nums[m/n][m%n] == target){
+                return true;
+            }else if(nums[m/n][m%n] > target)
+            return binary (nums, target, i, m, n);
+            else
+                return binary(nums, target, m+1, j, n);
+        }else return false;
+    }
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        return binary(matrix, target, 0, m * n, n);
+    }
+
+};
