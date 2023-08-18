36b4005e24bf59161e600e33f919d5aa440c1f13
Ruturaj Chintawar <52837894+bitscoder@users.noreply.github.com>
2023-08-18 00:21:45 -0700

    Time: 37 ms (83.39%), Space: 19 MB (18.36%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int check(vector<int>& nums, int k) {
+        int h = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            h += (nums[i] + k - 1) / k;
+        }
+        return h;
+    }
+
+    int binary(vector<int>& nums, int h, int i, int j) {
+        if (i < j) {
+            int m = i + (j - i) / 2;
+            int k = check(nums, m);
+            if (k <= h) {
+                return binary(nums, h, i, m);
+            } else {
+                return binary(nums, h, m + 1, j);
+            }
+        }
+        return i;
+    }
+
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int maxele = 0;
+        for (auto it : piles) {
+            maxele = max(maxele, it);
+        }
+
+        int lo = 1;
+        int hi = maxele;
+
+        return binary(piles, h, lo, hi);
+    }
+
+
+
+};
