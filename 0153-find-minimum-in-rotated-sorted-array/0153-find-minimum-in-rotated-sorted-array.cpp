7910480ad11ecfc5ae95f24a03131e331de8e2b9
Ruturaj Chintawar <52837894+bitscoder@users.noreply.github.com>
2023-08-19 01:07:18 -0700

    Time: 0 ms (100.00%), Space: 10.1 MB (92.24%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -1,10 +1,20 @@
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-        int minele = nums[0];
-        for(int i = 0;i < nums.size(); i++){
-            minele = min(minele, nums[i]);
-        }    
-        return minele;
+        return binary(nums, 0, nums.size() - 1);
     }
+
+    int binary(vector<int>& nums, int i, int j) {
+        if (i < j) {
+            int m = i + (j - i) / 2; // This avoids integer overflow
+
+            if (nums[m] > nums[j])
+                return binary(nums, m + 1, j);
+            else
+                return binary(nums, i, m);
+        } else {
+            return nums[i];
+        }
+    }
+
 };
