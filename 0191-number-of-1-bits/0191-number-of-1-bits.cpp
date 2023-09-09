6af1571d0451078084f61b5fa7cc30d7a1b44721
Ruturaj Chintawar <52837894+bitscoder@users.noreply.github.com>
2023-09-08 17:18:27 -0700

    Time: 0 ms (100.00%), Space: 6.2 MB (33.75%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -1,12 +1,18 @@
 class Solution {
 public:
-    int hammingWeight(uint32_t n) {
-        int ans = 0;
+    
+int hammingWeight(uint32_t n) {
+    int ans = 0;
 
-        while(n!=0){
-            if(n & 1) ans++;
-            n = n >> 1;
-        }   
-        return ans;
-    }
+    /*while(n!=0){
+        if(n & 1) ans++;
+        n = n >> 1;
+    } */
+
+    while(n!=0){
+        n = n &(n-1);
+        ans++;
+    }  
+    return ans;
+}
 };
