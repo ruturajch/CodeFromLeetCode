ce51af8972451965db8d71ed208ef5eb15c0913b
Ruturaj Chintawar <52837894+bitscoder@users.noreply.github.com>
2023-09-16 19:09:22 -0700

    Time: 612 ms (40.37%), Space: 130.7 MB (29.97%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1584-min-cost-to-connect-all-points/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1584-min-cost-to-connect-all-points.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1584-min-cost-to-connect-all-points/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1584-min-cost-to-connect-all-points.cpp
@@ -1,72 +1,31 @@
 class Solution {
 public:
-    typedef pair<int, int> P;
-    int minMST(vector<vector<P>>& adj, int V) {
-
-        priority_queue<P, vector<P>, greater<P>> pq;
-
-        pq.push({0, 0});
-
-        vector<bool> inMST(V, false);
-
-        int sum = 0;
-
-        while(!pq.empty()){
-
-            int wt = pq.top().first;
-            int node = pq.top().second;
-            pq.pop();
-
-            if(inMST[node]){
-                continue;
-            }
-
-            inMST[node] = true;
-            sum += wt;
-
-            for(auto &temp: adj[node]){
-
-                int adjNode = temp.first;
-                int adjWt = temp.second;
-
-                if(inMST[adjNode] == false){
-
-                    pq.push({adjWt, adjNode});
-
-                }
-            }
-
-        }
-
-        return sum;
-       
-    }
-    
-    
     int minCostConnectPoints(vector<vector<int>>& points) {
-        int V = points.size();
-
-        vector<vector<pair<int, int>>> adj(V);
-
-        for(int i = 0; i < V; i++ ){
-
-            for(int j = i+1; j < V; j++){
-
-                int x1 = points[i][0];
-                int y1 = points[i][1];
-
-                int x2 = points[j][0];
-                int y2 = points[j][1];
-
-                int d = abs(x1 - x2) + abs(y1 - y2);
-
-                adj[i].push_back({j, d});
-                adj[j].push_back({i, d});
+        int n = points.size();
+        vector<pair<int, int>> adjList[n];
+        for(int i = 0; i < n; i++){
+            for(int j = i+1; j < n; j++){
+                int distance = abs(points[i][0] - points[j][0])+abs(points[i][1] - points[j][1]);
+                adjList[i].push_back(make_pair(j, distance));
+                adjList[j].push_back(make_pair(i, distance));
             }
         }
-
-        return minMST(adj, V);
-
+        priority_queue<pair<int, int>, vector<pair<int, int>>,greater<pair<int, int>>> pq;
+        vector<int> tapVector(n, 0);
+        pq.push({0, 0});
+        int ansSum = 0;
         
+        while(!pq.empty()){
+            auto front = pq.top();
+            pq.pop();
+            if(tapVector[front.second] == 1) continue;
+            tapVector[front.second] = 1;
+            ansSum += front.first;
+            for(auto i : adjList[front.second]){
+                if(tapVector[i.first] == 0)
+                    pq.push({i.second, i.first});
+            } 
+        }
+        return ansSum;
     }
 };
