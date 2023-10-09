f698c398a788f40f2c2bba17ff2646deb6a4ccb6
Ruturaj Chintawar <52837894+bitscoder@users.noreply.github.com>
2023-10-09 15:55:19 -0700

    Time: 33 ms (14.16%), Space: 13 MB (94.39%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div787-cheapest-flights-within-k-stops/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div787-cheapest-flights-within-k-stops.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div787-cheapest-flights-within-k-stops/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div787-cheapest-flights-within-k-stops.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
-    int findCheapestPrice(int n, vector<vector<int>> &flights, int src, int dst, int k)
-    {
+    int findCheapestPrice(int n, vector<vector<int>> &flights, int src, int dst, int k) {
         vector<int> price(n, INT_MAX);
         price[src] = 0;
-        for(int i = 0; i < k+1; i++){
-            vector<int> tempPrice = price;
-            for(int j = 0; j < flights.size(); j++){
-                if(price[flights[j][0]] == INT_MAX)
-                    continue;
-                if(price[flights[j][0]] + flights[j][2] < tempPrice[flights[j][1]])
-                    tempPrice[flights[j][1]] = price[flights[j][0]] + flights[j][2];
+
+        for (int i = 0; i < k + 1; i++) {
+            vector<int> helperVector = price;
+            int e = flights.size();
+
+            for (int j = 0; j < e; j++) {
+                if (price[flights[j][0]] != INT_MAX) {
+                    if (price[flights[j][0]] + flights[j][2] < helperVector[flights[j][1]])
+                        helperVector[flights[j][1]] = price[flights[j][0]] + flights[j][2];
+                }
             }
-            price = tempPrice;
+
+            price = helperVector;
         }
+
         return price[dst] == INT_MAX ? -1 : price[dst];
     }
 };
