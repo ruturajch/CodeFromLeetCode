a53f8d4882b708fdffd39c24ac0746502cc9ad68
Ruturaj Chintawar <52837894+bitscoder@users.noreply.github.com>
2023-10-26 15:50:26 -0700

    Time: 63 ms (64.81%), Space: 73 MB (54.14%) - LeetHub

--- 1397-search-suggestions-system/1397-search-suggestions-system.cpp
+++ 1397-search-suggestions-system/1397-search-suggestions-system.cpp
@@ -1,25 +1,78 @@
-class Solution {
+class TreeNod
+{
+public:
+    TreeNod *children[26];
+    bool isWordComplete;
+    TreeNod()
+    {
+        for (int i = 0; i < 26; i++)
+        {
+            children[i] = NULL;
+        }
+        isWordComplete = false;
+    }
+};
+class Solution
+{
+private:
+    void dfs(TreeNod *currNode, string &ansWord, vector<string> &ansVector, int& count)
+    {
+        if(currNode->isWordComplete && count != 0){
+            count--;
+            ansVector.push_back(ansWord);
+        }else if(count == 0){
+            return;
+        }
+        for(int i = 0; i < 26; i++){
+            if(currNode->children[i] != NULL){
+                ansWord += (char)(i + 'a');
+                dfs(currNode->children[i], ansWord, ansVector, count);
+                ansWord.pop_back();
+            }
+        }
+        return;
+    }
+
 public:
     vector<vector<string>> suggestedProducts(vector<string> &products, string searchWord)
-{
-    sort(products.begin(), products.end());
-    vector<vector<string>> res;
-    string cur = "";
-    for (int i = 0; i < searchWord.size(); i++)
     {
-        cur += searchWord[i];
-        vector<string> temp;
-        for (int j = 0; j < products.size(); j++)
+        TreeNod *root = new TreeNod();
+        for (auto word : products)
         {
-            if (products[j].substr(0, i + 1) == cur)
+            TreeNod *currNode = root;
+            for (auto s : word)
             {
-                temp.push_back(products[j]);
+                if (currNode->children[s - 'a'] == NULL)
+                {
+                    currNode->children[s - 'a'] = new TreeNod();
+                }
+                currNode = currNode->children[s - 'a'];
             }
-            if (temp.size() == 3)
-                break;
+            currNode->isWordComplete = true;
         }
-        res.push_back(temp);
+
+        vector<vector<string>> ans;
+        string cur = "";
+        for (int i = 0; i < searchWord.size(); i++)
+        {
+            cur += searchWord[i];
+            vector<string> tempAns;
+            TreeNod *currNode = root;
+            bool flag = true;
+            for(auto s : cur){
+                if(currNode->children[s - 'a'] != NULL){
+                    currNode = currNode->children[s - 'a'];
+                }else{
+                    flag = false;
+                    break;
+                }
+            }
+            int maxWords = 3;
+            if(flag){
+                dfs(currNode, cur, tempAns, maxWords);
+            }
+            ans.push_back(tempAns);
+        }
+        return ans;
     }
-    return res;
-}
 };
