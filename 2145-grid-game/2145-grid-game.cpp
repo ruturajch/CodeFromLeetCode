51faafa9cb36f603b38a1f29f43a28efb4dcd118
Ruturaj Chintawar <52837894+bitscoder@users.noreply.github.com>
2023-10-27 12:10:43 -0700

    Time: 124 ms (64.31%), Space: 63 MB (65.2%) - LeetHub

---
+++ 2145-grid-game/2145-grid-game.cpp
@@ -0,0 +1,25 @@
+
+class Solution {
+public:
+    long long gridGame(vector<vector<int>>& grid) {
+  
+        // prefix sum
+        long long top = grid[0][0],bottom = 0, answer = LONG_MAX;
+        for(int i =1;i<grid[0].size();i++){
+            top += grid[0][i];
+        }
+
+        for(int i =0;i<grid[0].size();i++){
+            // All the top points 2nd robot can collect
+            top -= grid[0][i];
+
+            // min because first robot wants to  minimize 
+            answer = min(answer,max(top,bottom));
+
+            // All the bootom points the robot could collect
+            bottom += grid[1][i];
+        }
+
+        return answer;
+    }
+};
