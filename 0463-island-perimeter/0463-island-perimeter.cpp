f920931abee2803fa12ed52c1f46a988dc7b46a1
Ruturaj Chintawar <52837894+bitscoder@users.noreply.github.com>
2023-11-01 23:22:41 -0700

    Time: 86 ms (45.61%), Space: 96.4 MB (72.04%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.cpp
+++ 0463-island-perimeter/0463-island-perimeter.cpp
@@ -1,46 +1,25 @@
-class Solution
-{
-private:
-    int dfs(vector<vector<int>> &grid, int n, int m, int r, int c, vector<vector<int>> &visited)
-    {
-        if (r < 0 || c < 0 || r >= n || c >= m || grid[r][c] == 0 || visited[r][c] == 1)
-        {
-            return 0;
-        }
-        int dim[4][2] = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
-        int ans = 0;
-        for (int i = 0; i < 4; i++)
-        {
-            int nrow = r + dim[i][0];
-            int ncol = c + dim[i][1];
-            if (nrow < 0 || ncol < 0 || nrow >= n || ncol >= m || grid[nrow][ncol] == 0)
-                ans++;
-            
-        }
-        visited[r][c] = 1;
-        for (int i = 0; i < 4; i++)
-        {
-            int nrow = r + dim[i][0];
-            int ncol = c + dim[i][1];
-            ans += dfs(grid, n, m, nrow, ncol, visited);
-        }
-        cout << r << " " << c << ans << endl;
-        return ans;
-    }
-
+class Solution {
 public:
-    int islandPerimeter(vector<vector<int>> &grid)
-    {
-        int n = grid.size();
-        int m = grid[0].size();
-        vector<vector<int>> visited(n, vector<int>(m, 0));
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(grid[i][j] ==  1){
-                    return dfs(grid, n, m, i, j, visited);
-                }
-            }
+    int islandPerimeter(vector<vector<int>>& grid) {
+      int m = grid.size();
+      int n = grid[0].size();
+      
+      int prem = 0;
+      
+      for(int i = 0 ; i < m ; i++)
+      {
+        for(int j = 0 ; j < n ; j++)
+        {
+          if(grid[i][j] == 1) {
+            prem += 4;
+          
+            if(j > 0 && grid[i][j-1] == 1) prem -=2;
+            if(i >0 && grid[i-1][j] == 1) prem -=2;
+
+          }
         }
-        return 0;
+      }
+      
+      return prem;
     }
 };
