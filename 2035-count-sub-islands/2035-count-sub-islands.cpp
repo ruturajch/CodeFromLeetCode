6543db30e03d19e9768643982a619a23ff881eb6
Ruturaj Chintawar <52837894+bitscoder@users.noreply.github.com>
2023-11-03 00:47:18 -0700

    Time: 266 ms (41.96%), Space: 108.9 MB (32.27%) - LeetHub

--- 2035-count-sub-islands/2035-count-sub-islands.cpp
+++ 2035-count-sub-islands/2035-count-sub-islands.cpp
@@ -1,37 +1,46 @@
-class Solution {
+class Solution
+{
+private:
+    bool dfs(vector<vector<int>> &grid1, vector<vector<int>> &grid2, int r, int c, int n, int m, vector<vector<int>> &helperVector)
+    {
+        if (r < 0 || c < 0 || r >= n || c >= m || grid2[r][c] == 0 || helperVector[r][c] == 1)
+        {
+            return true;
+        }
+        helperVector[r][c] = 1;
+        bool ans = true;
+        if (grid1[r][c] == 0)
+        {
+            ans = false;
+        }
+        int dim[4][2] = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
+        for (int i = 0; i < 4; i++)
+        {
+            int nrow = r + dim[i][0];
+            int ncol = c + dim[i][1];
+            ans = ans & dfs(grid1, grid2, nrow, ncol, n, m, helperVector);
+        }
+        return ans;
+    }
+
 public:
-    int countSubIslands(vector<vector<int>>& grid1, vector<vector<int>>& grid2) {
-        const int ROWS = grid1.size(), COLS = grid1[0].size();
-        set<int> visit;
-        
-        function<bool(int, int)> dfs = [&] (int r, int c) -> bool {
-            if (
-                r < 0
-                || c < 0
-                || r == ROWS
-                || c == COLS
-                || grid2[r][c] == 0
-                || visit.count(r*COLS + c)
-            )
-                return true;
-            
-            visit.insert(r*COLS + c);
-            bool res = true;
-            if(grid1[r][c] == 0)
-                res = false;
-            
-            res = dfs(r - 1, c) && res;
-            res = dfs(r + 1, c) && res;
-            res = dfs(r, c - 1) && res;
-            res = dfs(r, c + 1) && res;
-            return res;
-        };
-        
-        int count = 0;
-        for(int r = 0; r < ROWS; r++)
-            for(int c = 0; c < COLS; c++)
-                if(grid2[r][c] && !visit.count(r*COLS + c) && dfs(r, c))
-                    count += 1;
-        return count;
+    int countSubIslands(vector<vector<int>> &grid1, vector<vector<int>> &grid2)
+    {
+        int n = grid1.size();
+        int m = grid1[0].size();
+        vector<vector<int>> helperVector(n, vector<int>(m, 0));
+        int ans = 0;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                if (helperVector[i][j] == 0 && grid2[i][j] == 1 && dfs(grid1, grid2, i, j, n, m, helperVector))
+                {
+                    cout << i << j << endl;
+                    ans++;
+                }
+            }
+        }
+        return ans;
     }
 };
