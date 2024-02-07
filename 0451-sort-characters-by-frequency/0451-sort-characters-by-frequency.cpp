5064a40414cfa18bc844c13c7756fe4135a0060a
Ruturaj Chintawar <52837894+bitscoder@users.noreply.github.com>
2024-02-08 01:44:31 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,35 +1,34 @@
 class Solution {
 private:
-    static bool check(pair<int, int> a, pair<int, int> b) {
-        if (a.first == b.first) {
+    static bool check(pair<int, int> a, pair<int, int> b){
+        if(a.first == b.first){
             return a.second < b.second;
-        } else {
+        }else{
             return a.first > b.first;
         }
     }
-
 public:
     string frequencySort(string s) {
         unordered_map<char, int> um;
-        for (char ch : s) {
+        for(char ch : s){
             um[ch]++;
         }
+        vector<pair<int, char>> vec;
 
-        vector<pair<int, char>> vec; // Adjusted the pair type
-
-        for (auto it : um) {
-            vec.push_back({it.second, it.first}); // Fix the order of elements here
+        for(auto it : um){
+            vec.push_back({it.second, it.first});
         }
-
+        
         sort(vec.begin(), vec.end(), check);
 
         string result = "";
 
-        for (auto it : vec) { // Iterate over the sorted vector
-            cout << string(it.first, it.second) << endl;
-            result += string(it.first, it.second);
+        for(auto it : vec){
+            cout << string(it.second, it.first) << endl;
+            result += string(it.second, it.first);
         }
-
         return result;
     }
 };
+
+
