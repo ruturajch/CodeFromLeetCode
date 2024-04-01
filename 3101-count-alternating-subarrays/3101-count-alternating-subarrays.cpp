0e4a16c41bd9842f4672c7f27c4038b202177f41
Ruturaj Chintawar <52837894+bitscoder@users.noreply.github.com>
2024-03-31 17:42:34 -0700

    Time: 91 ms (57.14%), Space: 116.1 MB (57.14%) - LeetHub

--- 3101-count-alternating-subarrays/3101-count-alternating-subarrays.cpp
+++ 3101-count-alternating-subarrays/3101-count-alternating-subarrays.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     long long countAlternatingSubarrays(vector<int>& nums) {
-        //vector<int> dp(nums.size(), 0);
         long long ansCount = 1;
         int it = 0;
-        for(int i = 1; i < nums.size(); i++){
+        int n = nums.size();
+        for(int i = 1; i < n ; i++){
             if(nums[i] == !nums[i - 1]){
                 ansCount += i - it + 1;
             }else{
