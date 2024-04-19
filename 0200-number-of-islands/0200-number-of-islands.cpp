62503b4d6737f97e2054b3f4b80b66b7edcfcba9
Ruturaj Chintawar <52837894+bitscoder@users.noreply.github.com>
2024-04-18 21:31:15 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,44 +1,54 @@
 class Solution {
 public:
-    int numIslands(vector<vector<char>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
+   void bfs(vector<vector<int>>& helperVector, int k, int m, vector<int> adjList[]) {
+    queue<int> q;
+    q.push(k);
+    while (!q.empty()) {
+        int cur = q.front();
+        q.pop();
+        int i = cur / m;
+        int j = cur % m;
+        helperVector[i][j] = 1;
+        for (int next : adjList[cur]) {
+            int ni = next / m;
+            int nj = next % m;
+            if (helperVector[ni][nj] == 0) {
+                q.push(next);
+            }
+        }
+    }
+}
 
-        int ans = 0;
-        
-        vector<vector<bool>> vis(n, vector<bool> (m,false));
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(grid[i][j]=='1' && !vis[i][j]){
-                    ans++;
-                    queue<pair<int,int>> q;
-                    q.push({i,j});
-                    vis[i][j] = true;
-                    while(!q.empty()){
-                        int row = q.front().first;
-                        int col = q.front().second;
-                        q.pop();
+int numIslands(vector<vector<char>>& grid) {
+    int n = grid.size();
+    int m = grid[0].size();
 
-                        if( row!=0 && grid[row-1][col] == '1' && !vis[row-1][col]){
-                            q.push({row-1,col});
-                            vis[row-1][col] = true;
-                        }
-                        if( col!=0 && grid[row][col-1] == '1' && !vis[row][col-1]){
-                            q.push({row,col-1});
-                            vis[row][col-1] = true;
-                        }
-                        if( row!=n-1 && grid[row+1][col] == '1' && !vis[row+1][col]){
-                            q.push({row+1,col});
-                            vis[row+1][col] = true;
-                        }
-                        if( col!=m-1 && grid[row][col+1] == '1' && !vis[row][col+1]){
-                            q.push({row,col+1});
-                            vis[row][col+1] = true;
-                        }
+    int sides[4][2] = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+    vector<int> adjList[m * n];
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (grid[i][j] == '1') {
+                for (int k = 0; k < 4; k++) {
+                    int nrow = i + sides[k][0];
+                    int ncol = j + sides[k][1];
+                    if (nrow >= 0 && ncol >= 0 && nrow < n && ncol < m && grid[nrow][ncol] == '1') {
+                        adjList[i * m + j].emplace_back(nrow * m + ncol);
                     }
                 }
             }
         }
-        return ans;
     }
+    vector<vector<int>> helperVector(n, vector<int>(m, 0));
+    int ans = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (helperVector[i][j] == 0 && grid[i][j] == '1') {
+                ans++;
+                bfs(helperVector, i * m + j, m, adjList);
+            }
+        }
+    }
+    return ans;
+}
+
 };
