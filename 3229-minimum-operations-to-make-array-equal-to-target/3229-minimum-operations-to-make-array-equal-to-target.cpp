4f4882457ec1a121d8453fb2cd693004f1a177a7
Ruturaj Chintawar <52837894+ruturajch@users.noreply.github.com>
2024-07-22 10:53:26 -0700

    Time: 141 ms (100%), Space: 122.2 MB (100%) - LeetHub

--- 3229-minimum-operations-to-make-array-equal-to-target/3229-minimum-operations-to-make-array-equal-to-target.cpp
+++ 3229-minimum-operations-to-make-array-equal-to-target/3229-minimum-operations-to-make-array-equal-to-target.cpp
@@ -1,12 +1,27 @@
 class Solution {
 public:
-    long long minimumOperations(vector<int>& A, vector<int>& target) {
-        long res = 0;
-        int pre = 0, n = A.size();
-        for (int i = 0; i < n; ++i) {
-            res += max(target[i] - A[i] - pre, 0);
-            pre = target[i] - A[i];
+    long long minimumOperations(vector<int>& nums, vector<int>& target) {
+        int n = nums.size();
+        long incr = 0, decr = 0, ops = 0;
+
+        for (int i = 0; i < n; i++) {
+            long diff = target[i] - nums[i];
+
+            if (diff > 0) {
+                if (incr < diff)
+                    ops += diff - incr;
+                incr = diff;
+                decr = 0;
+            } else if (diff < 0) {
+                if (diff < decr)
+                    ops += decr - diff;
+                decr = diff;
+                incr = 0;
+            } else {
+                incr = decr = 0;
+            }
         }
-        return res + max(-pre, 0);
+
+        return ops;
     }
 };
