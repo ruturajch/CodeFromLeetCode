56fdd3f118bb83a05373077dbfdc14b052913f8e
Ruturaj Chintawar <52837894+ruturajch@users.noreply.github.com>
2024-08-12 13:54:42 -0700

    Time: 0 ms (100%), Space: 13.9 MB (21.37%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -3,24 +3,20 @@ public:
     int search(vector<int>& a, int target) {
         int n=a.size();
         int l=0, r=n-1;
-        
         while(l<=r){
             int m=(l+r)/2;
             if(a[m]==target) return m;
-            if(a[m]>a[n-1]){ // left side of peak element
-                if(target>a[m] || target<a[0]){
+            if(a[m]>a[n-1])
+                if(target>a[m] || target<a[0])
                     l=m+1;
-                }else{
+                else
                     r=m-1;
-                }
-            }else{ // right side of peak element
-                if(target<a[m] || target>a[n-1]){
+            else
+                if(target<a[m] || target>a[n-1])
                     r=m-1;
-                }else{
+                else
                     l=m+1;
-                }
-            }
         }
-      return -1;
+        return -1;
     }
 };
