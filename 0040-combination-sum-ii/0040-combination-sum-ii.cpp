77cb9e847d2624b11d88d59a2d0c4b768ef240fd
Ruturaj Chintawar <52837894+ruturajch@users.noreply.github.com>
2024-08-12 20:00:07 -0700

    Time: 9 ms (21.7%), Space: 13 MB (38.57%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,21 +1,21 @@
 class Solution {
 private:
     void backtrack(vector<int>& candidates, int target, int start, vector<int>& current, vector<vector<int>>& result) {
-    if (target == 0) {
-        result.push_back(current);
-        return;
-    }else if(target < 0 || candidates.size() <= start) return;
+        if (target == 0) {
+            result.push_back(current);
+            return;
+        }else if(target < 0 || candidates.size() <= start) 
+            return;
 
-    current.push_back(candidates[start]);
-    backtrack(candidates, target - candidates[start], start+1, current, result);
-    current.pop_back(); // Backtrack by removing the last element
-
-    while(start+1 < candidates.size() && candidates[start] == candidates[start+1]){
-        start++;
+        current.push_back(candidates[start]);
+        backtrack(candidates, target - candidates[start], start+1, current, result);
+        current.pop_back();
+        
+        while(start+1 < candidates.size() && candidates[start] == candidates[start+1])
+            start++;
+        
+        backtrack(candidates, target, start+1, current, result);
     }
-    backtrack(candidates, target, start+1, current, result);
-    
-}
 public:
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
         sort(candidates.begin(), candidates.end()); // Sort candidates to handle duplicates
