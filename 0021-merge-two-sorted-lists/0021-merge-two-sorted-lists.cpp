60f7ea34bd2a5a0c66644aeb0b4dc37058b54cf8
Ruturaj Chintawar <52837894+ruturajch@users.noreply.github.com>
2024-08-12 20:41:41 -0700

    Time: 8 ms (24.44%), Space: 20.2 MB (5.71%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -9,53 +9,36 @@
  * };
  */
 class Solution {
+private:
+    static bool compare(ListNode* list1, ListNode* list2){
+        return list1->val < list2->val;
+    }
 public:
      ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-       
-	    // if list1 happen to be NULL
-		// we will simply return list2.
-        if(list1 == NULL)
-            return list2;
-		
-		// if list2 happen to be NULL
-		// we will simply return list1.
-        if(list2 == NULL)
-            return list1;
+
+        vector<ListNode*> helperVector;
+        while(list1 != nullptr){
+            helperVector.push_back(list1);
+            list1 = list1->next;
+        }
+        while(list2 != nullptr){
+            helperVector.push_back(list2);
+            list2 = list2->next;
+        }
         
-        ListNode * ptr = list1;
-        if(list1 -> val > list2 -> val)
-        {
-            ptr = list2;
-            list2 = list2 -> next;
+        sort(helperVector.begin(), helperVector.end(), [](ListNode* list1, ListNode* list2) {
+            return list1->val < list2->val;
+        });
+
+        for(int i = 1; i < helperVector.size(); i++){
+            helperVector[i-1]->next = helperVector[i];
         }
-        else
-        {
-            list1 = list1 -> next;
-        }
-        ListNode *curr = ptr;
         
-		// till one of the list doesn't reaches NULL
-        while(list1 &&  list2)
-        {
-            if(list1 -> val < list2 -> val){
-                curr->next = list1;
-                list1 = list1 -> next;
-            }
-            else{
-                curr->next = list2;
-                list2 = list2 -> next;
-            }
-            curr = curr -> next;
-                
+        if(helperVector.size() == 0)
+            return nullptr;
+        else{
+            helperVector[helperVector.size()-1]->next = nullptr;
+            return helperVector[0];
         }
-		
-		// adding remaining elements of bigger list.
-        if(!list1)
-            curr -> next = list2;
-        else
-            curr -> next = list1;
-            
-        return ptr;
-       
     }
 };
