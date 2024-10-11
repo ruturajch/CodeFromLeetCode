0974b8a98c40e7f44a7bf37cac0524eb477e6132
Ruturaj Chintawar <52837894+ruturajch@users.noreply.github.com>
2024-10-10 21:18:50 -0700

    Time: 159 ms (45.36%), Space: 63.5 MB (52.92%) - LeetHub

--- 1942-the-number-of-the-smallest-unoccupied-chair/1942-the-number-of-the-smallest-unoccupied-chair.cpp
+++ 1942-the-number-of-the-smallest-unoccupied-chair/1942-the-number-of-the-smallest-unoccupied-chair.cpp
@@ -1,57 +1,33 @@
 class Solution {
 public:
-    int smallestChair(std::vector<std::vector<int>>& times, int targetFriend) {
-        int n = times.size();
-        
-        // Create a list of events with (time, friend, type_of_event)
-        std::vector<std::tuple<int, int, int>> events;
-        for (int i = 0; i < n; ++i) {
-            events.emplace_back(times[i][0], i, 1); // Arrival event
-            events.emplace_back(times[i][1], i, 0); // Leaving event
+    int smallestChair(vector<vector<int>>& times, int targetFriend) {
+        // adding the index
+        for(int i = 0; i < times.size(); i++){
+            times[i].push_back(i);
         }
-
-        // Sort events by time, and if equal time, process leaving before arrival
-        std::sort(events.begin(), events.end(), [](auto& a, auto& b) {
-            if (std::get<0>(a) == std::get<0>(b)) {
-                return std::get<2>(a) < std::get<2>(b); // Leave first
-            }
-            return std::get<0>(a) < std::get<0>(b);
+        // sort
+        sort(times.begin(), times.end(), [](vector<int>& a, vector<int>& b){
+            return a[0] < b[0];
         });
-
-        // Priority queue to keep track of available chairs (smallest first)
-        std::priority_queue<int, std::vector<int>, std::greater<int>> availableChairs;
-        for (int i = 0; i < n; ++i) availableChairs.push(i); // Initially all chairs available
-
-        // Map to store which friend sits on which chair
-        std::vector<int> assignedChairs(n, -1);
-
-        // Process events
-        for (auto& event : events) {
-            int time = std::get<0>(event);
-            int friendIdx = std::get<1>(event);
-            int eventType = std::get<2>(event);
-
-            if (eventType == 1) { // Arrival
-                int chair = availableChairs.top();
-                availableChairs.pop();
-                assignedChairs[friendIdx] = chair;
-
-                // If the current friend is the target friend, return the chair number
-                if (friendIdx == targetFriend) {
-                    return chair;
-                }
-            } else { // Leaving
-                availableChairs.push(assignedChairs[friendIdx]); // Chair becomes available
+        priority_queue<int, vector<int>, greater<int>> helperOne;
+        for (int i = 0; i < times.size(); i++) {
+            helperOne.push(i);
+        }
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> helperTwo;
+        //cout << "ruturaj" << endl;
+        // Output the
+        for(int i = 0; i < times.size(); i++){
+            while(!helperTwo.empty() &&helperTwo.top().first <= times[i][0]){
+                helperOne.push(helperTwo.top().second);
+                helperTwo.pop();
             }
+            if(times[i][2] == targetFriend){
+                return helperOne.top();
+            }
+            helperTwo.push({times[i][1],helperOne.top()});
+            helperOne.pop();
         }
 
-        return -1; // Shouldn't happen if input is valid
+        return 0;
     }
 };
-
-
-// 1, 4
-// 2, 3
-// 4, 6
-// targetFriend = 1
-// 
