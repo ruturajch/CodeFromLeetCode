ebe007832ebe6310879538f0efe6f59f29bed4bf
Ruturaj Chintawar <52837894+ruturajch@users.noreply.github.com>
2024-10-13 19:38:34 -0700

    Time: 44 ms (57.27%), Space: 23.8 MB (18.99%) - LeetHub

---
+++ 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.cpp
@@ -0,0 +1,58 @@
+class Solution {
+private:
+    bool check(set<int> expectedSet, unordered_map<int, int> helperMap){
+        bool flag = true;
+        for(auto it : expectedSet){
+            if(helperMap[it]!= 0){
+                expectedSet.erase(it);
+            }else{
+                flag = false;
+            }
+        }
+        return flag;
+    }
+
+public:
+    vector<int> smallestRange(vector<vector<int>>& nums) {
+        vector<pair<int, int>> helperVector;
+        for(int i = 0; i < nums.size(); i++)
+        {   
+            for(auto it : nums[i]){
+                helperVector.push_back({it, i});
+            }
+        }
+        sort(helperVector.begin(), helperVector.end());
+        
+        set<int> expectedSet;
+        for(int i = 0; i < nums.size(); i++){
+            expectedSet.insert(i);
+        }
+
+        int l = 0;
+        int r = 0;
+        int ans = helperVector[helperVector.size()-1].first - helperVector[0].first;
+        
+        vector<int> ansVec;
+        ansVec.push_back(helperVector[0].first);
+        ansVec.push_back(helperVector[helperVector.size()-1].first);
+        unordered_map<int, int> helperMap;
+        while(r < helperVector.size()){
+            helperMap[helperVector[r].second]++;
+            expectedSet.erase(helperVector[r].second);
+            while(expectedSet.empty()){
+                if((helperVector[r].first-helperVector[l].first) < ans){
+                    ansVec[0] = helperVector[l].first;
+                    ansVec[1] = helperVector[r].first;
+                    ans = helperVector[r].first-helperVector[l].first;
+                }
+                helperMap[helperVector[l].second]--;
+                if(helperMap[helperVector[l].second] == 0){
+                    expectedSet.insert(helperVector[l].second);
+                }
+                l++;
+            }
+            r++;
+        }
+        return ansVec;
+    }
+};
